90685cbb622ca1383104704b31411685055c959d
ricardo4lvarez <ricardo4lvarez.m@gmail.com>
2025-11-20 16:44:08 -0600

    Análisis bivarable

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ churn_1_check = True
 # Leer los datos del archivo CSV
 df = pd.read_csv("Database/Customer-Churn-Records.csv")
 
+#%%% Intro
 """
 # Análisis de clientes bancarios que dimitieron
 
@@ -21,7 +22,7 @@ Ya realicé el EDA de este proyecto, por lo que recomiendo vayan al repositorio 
 Voy a comenzar identificando cuántos de los usuarios han dimitido y cuántos no.
 """
 
-#%% Limpieza EDA
+#%% Limpieza
 df.drop(columns=["RowNumber", "CustomerId", "Surname"], axis=1, inplace=True)
 df.rename(columns={"Exited":"Churn"}, inplace=True)
 variables_cat = ["Tenure", "Geography", "Gender", "NumOfProducts", "HasCrCard", "IsActiveMember", "Complain", "Satisfaction Score", "Card Type"]
@@ -63,17 +64,18 @@ ax = sns.countplot(
 plt.title("Churn counts")
 
 if churn_1_check & churn_0_check:
-    plt.legend(['No', 'Si'])
-elif churn_1_check & churn_0_check == False:
-    plt.legend(['Si'])
-elif churn_1_check == False & churn_0_check:
+    plt.legend(['No', 'Yes'])
+elif not churn_0_check & churn_1_check:
     plt.legend(['No'])
+elif not churn_1_check & churn_0_check:
+    plt.legend(['Yes'])
 
 for container in ax.containers:
     ax.bar_label(container)
 st.pyplot(fig)
 
 """
+# Análisis univariable
 Ahora vamos a iniciar con un análisis univariable de las columnas categóricas respecto a los usuarios que dimitieron y los que no 
 
 NOTA: 1 significa que dimitieron, 0 significa que siguen usando el servicio.
@@ -87,7 +89,7 @@ for col, predicor in enumerate(df_filtrado.select_dtypes("category")):
     st.pyplot(fig)
 
 #%% Insigths del análisis univariable
-"""### Insights
+"""## Insights
 
 - Podemos observar que el grupo de edad que más *abandona* 30 años a los 60 años.
 - Los productos 3 y 4 tienen más personas que *abandonan* que usuarios activos casi en un 100%.
@@ -99,6 +101,7 @@ for col, predicor in enumerate(df_filtrado.select_dtypes("category")):
 #%%% KDA plots
 
 """
+#Análisis univariable
 Ahora vamos a iniciar con un análisis univariable de las columnas numéricas respecto a los usuarios que dimitieron y los que no 
 
 NOTA: 1 significa que dimitieron, 0 significa que siguen usando el servicio.
@@ -106,13 +109,24 @@ NOTA: 1 significa que dimitieron, 0 significa que siguen usando el servicio.
 """
 
 for i, predicor in enumerate(df_filtrado.select_dtypes("number")):
+    if predicor == 'Churn':
+        continue
+    
     fig = plt.figure()
     sns.kdeplot(data=df_filtrado, x=predicor, hue="Churn")
-    plt.legend(["Churn", "No Churn"])
+    
+    #Configurar la leyenda dependiendo del filtro
+    if churn_1_check & churn_0_check:
+        plt.legend(['No', 'Yes'])
+    elif churn_1_check & churn_0_check == False:
+        plt.legend(['No'])
+    elif churn_1_check == False & churn_0_check:
+        plt.legend(['Yes'])
+
     st.pyplot(fig)
 
 """
-### Insights
+## Insights
 
 - Podemos observar que el grupo de edad que más *abandona* 30 años a los 60 años.
 - Los productos 3 y 4 tienen más personas que *abandonan* que usuarios activos casi en un 100%.
@@ -120,5 +134,38 @@ for i, predicor in enumerate(df_filtrado.select_dtypes("number")):
 - Las personas que viven en Alemania *abandonan* la compañía con más frecuencia.
 - Las mujeres tienen a *abandonar* los servicios más que los hombres.
 """
+
+#%% Churn vs no churn análisis
+
+"""
+# Análisis bivariable
+
+La primer gráfica siempre va a ser la de aquellos que han dimitido y la segunda los que no.
+"""
+
+grupo1 = df[df["Churn"] == 1]
+grupo2 = df[df["Churn"] == 0]
+
+fig = plt.figure(figsize=(10,6))
+ax = sns.countplot(data=grupo1, x="Gender", hue="Geography", palette="pastel", edgecolor="black")
+plt.title("Churners count by Gender and Geography")
+for container in ax.containers:
+    ax.bar_label(container)
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(10,5))
+ax = sns.countplot(data=grupo2, x="Gender", hue="Geography", palette="pastel", edgecolor="black")
+plt.title("NO churners count by Gender and Geography")
+for container in ax.containers:
+    ax.bar_label(container)
+st.pyplot(fig)
+
+"""
+## Insights
+
+- Observamos que Alemania y Francia tienen la mayor cantidad de personas que *abandonan* la compañía, sin embargo, proporcionalmente Alemania es mayor su taza de abandono, 37.2% para las mujeres y 27.8% para los hombres.
+- Volvemos a apreciar que las mujeres son más propensas a *abandonar*que los hombres.
+"""
+
 #%% Análisis de correlación
 
